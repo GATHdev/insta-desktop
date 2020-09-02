97d6a05e2d313b4c3ba1d13a7282dd8b718c790d
gatheesha <gatheesha.official@gmail.com>
2020-09-02 21:21:52 +0530

    disabled menu bar

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron')
+const { webContents } = require('electron')
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width:360,
     height: 640,
-    frame: false,
+    frame: true,
     backgroundColor: '#2e2c29',
+    autoHideMenuBar: 'true',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -19,12 +21,13 @@ function createWindow () {
   userAgent:'Mozilla/5.0 (Linux; Android 7.0; SM-G930VC Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36'
   })
 
+
+
   const ses = mainWindow.webContents.session
   console.log(ses.getUserAgent())
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
